a51de45d8221744d27cf9e56a6b20e5672a0e7eb
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2019-07-03 21:42:31 +0200

    Declare return types on tests

--- tests/Blade/BladeIncludeFirstTest.php
+++ tests/Blade/BladeIncludeFirstTest.php
@@ -4,7 +4,7 @@ namespace PrinsFrank\IndentingPersistentBladeCompiler\Tests\Blade;
 
 class BladeIncludeFirstTest extends AbstractBladeTestCase
 {
-    public function testIncludeFirstsAreCompiled()
+    public function testIncludeFirstsAreCompiled(): void
     {
         $this->assertEquals('<?php echo $__env->first(["one", "two"], \Illuminate\Support\Arr::except(get_defined_vars(), [\'__data\', \'__path\']) + [\'indenting\' => \'\'])->render(); ?>', $this->compiler->compileString('@includeFirst(["one", "two"])'));
         $this->assertEquals('<?php echo $__env->first(["one", "two"], ["foo" => "bar"], \Illuminate\Support\Arr::except(get_defined_vars(), [\'__data\', \'__path\']) + [\'indenting\' => \'\'])->render(); ?>', $this->compiler->compileString('@includeFirst(["one", "two"], ["foo" => "bar"])'));
--- tests/Blade/BladeIncludeWhenTest.php
+++ tests/Blade/BladeIncludeWhenTest.php
@@ -4,7 +4,7 @@ namespace PrinsFrank\IndentingPersistentBladeCompiler\Tests\Blade;
 
 class BladeIncludeWhenTest extends AbstractBladeTestCase
 {
-    public function testIncludeWhensAreCompiled()
+    public function testIncludeWhensAreCompiled(): void
     {
         $this->assertEquals('<?php echo $__env->renderWhen(true, \'foo\', ["foo" => "bar"], \Illuminate\Support\Arr::except(get_defined_vars(), [\'__data\', \'__path\']) + [\'indenting\' => \'\']); ?>', $this->compiler->compileString('@includeWhen(true, \'foo\', ["foo" => "bar"])'));
         $this->assertEquals('<?php echo $__env->renderWhen(true, \'foo\', \Illuminate\Support\Arr::except(get_defined_vars(), [\'__data\', \'__path\']) + [\'indenting\' => \'\']); ?>', $this->compiler->compileString('@includeWhen(true, \'foo\')'));
