d95ff93d320baeb09ba44bcd02e91efd1c3b6e4c
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2019-07-07 14:18:10 +0200

    Fixed issue where indenting was added on empty lines

--- src/Helpers/ContentHelper.php
+++ src/Helpers/ContentHelper.php
@@ -5,15 +5,21 @@ namespace PrinsFrank\IndentingPersistentBladeCompiler\Helpers;
 class ContentHelper
 {
     /**
+     * This function matches any newline if that line contains content and adds padding
+     * in front of the content so no useless padding is added for empty lines
+     *
      * @param string $content
      * @param $indenting
      */
     public static function addIndentingEachLine(string &$content, string $indenting): void
     {
-        $content = preg_replace('/(\r\n|\r|\n)/', '$1'.$indenting, $content);
+        $content = preg_replace('/(\r\n|\r|\n)(.)/', '$1'.$indenting.'$2', $content);
     }
 
     /**
+     * If no newline is found at the end of the string, add a newline character
+     * If we don't do this, indenting breaks with the next content
+     *
      * @param string $content
      */
     public static function addLastNewLineIfMissing(string &$content): void
