1f1f74810de2b488722003cf04b085658078dda3
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2019-07-17 23:08:36 +0200

    Added tests for ExpressionHelper

---
+++ tests/Helpers/ExpressionHelperTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace PrinsFrank\IndentingPersistentBladeCompiler\Tests\Helpers;
+
+use PHPUnit\Framework\TestCase;
+use PrinsFrank\IndentingPersistentBladeCompiler\Helpers\ExpressionHelper;
+
+/**
+ * Class ContentHelperTest
+ * @package PrinsFrank\IndentingPersistentBladeCompiler\Tests\Helpers
+ *
+ * @coversDefaultClass \PrinsFrank\IndentingPersistentBladeCompiler\Helpers\ExpressionHelper
+ */
+class ExpressionHelperTest extends TestCase
+{
+    /**
+     * @covers ::addIndenting
+     */
+    public function testAddsIndentingToExpression(): void
+    {
+        $expression = '()';
+        ExpressionHelper::addIndenting($expression, '    ');
+        $this->assertEquals("(, '    ')", $expression);
+
+        $expression = "('foo')";
+        ExpressionHelper::addIndenting($expression, '    ');
+        $this->assertEquals("('foo', '    ')", $expression);
+    }
+}
